36e7a17846c4d5de192392dedcec565cc62684e9
julie <julie@DESKTOP-5L3T3C6>
2023-04-25 11:05:47 -0500

    After class Tuesday, April 25

--- src/finalMaterial/week14_25Apr_tues/JMLExample.java
+++ src/finalMaterial/week14_25Apr_tues/JMLExample.java
@@ -5,13 +5,21 @@ public class JMLExample {
     //@ensures \result == x+1;
     //@ensures \result >= 1;
     public static int plusOne(int x) {
+        if (x < 0) {
+            //precondition not met, throw exception
+            throw new IllegalArgumentException("precondition failed: parameters must be nonnegative");
+        }
+
         int ans = x + 1;
+
+        assert ans == x+1 : "postcondition failed: should return parameter plus 1";
+        assert ans >= 1 : "postcondition failed: should return something >=1";
 
         return ans;
     }
 
     //@requires nums != null;
-    //@ensures (\forall int k; 0 <= k && k < nums.length; nums[k] == \old(nums[k]) + 1);
+    //@ensures (\forall int k; 0 <= k && k < nums.length; nums[k] == \old(nums[k]) * 2);
     public static void doubleEach(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             nums[i] = nums[i] * 2;
--- src/finalMaterial/week14_25Apr_tues/JMLExampleTest.java
+++ src/finalMaterial/week14_25Apr_tues/JMLExampleTest.java
@@ -7,6 +7,14 @@ class JMLExampleTest {
 
     @Test
     void plusOne() {
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    JMLExample.plusOne(-1);
+                }, "should get exception if I pass a negative value");
+
+        //test the postconditions and return of the method
+        int back = JMLExample.plusOne(5);
+        assertEquals(back, 6, "back should be 6");
     }
 
     @Test
