6fae50aa683836e2af3272a857b8f0a2393f4695
julie <julie@DESKTOP-5L3T3C6>
2023-05-02 10:47:10 -0500

    After class Tues May 2

--- src/finalMaterial/week15_2May_tues/OpenJMLTest.java
+++ src/finalMaterial/week15_2May_tues/OpenJMLTest.java
@@ -9,26 +9,24 @@ public class OpenJMLTest {
         return a + 1;
     }
 
+    //@ requires x <= Integer.MAX_VALUE - 2;
     //@ ensures \result == x + 2;
-    public static int same(int x) {
+    public static int addTwo(int x) {
         int temp = add(x);
         return add(temp);
     }
 
-/*
     //@ requires a != null;
-    //@ ensures (\forall int k; 0 <= k && k <= a.length; a[k] == k);
+    //@ ensures (\forall int k; 0 <= k && k < a.length; a[k] == k);
     public void setToIndex(int[] a) {
-        //@ loop_invariant 0 <= i && i < a.length;
+        //@ loop_invariant 0 <= i && i <= a.length;
         //@ loop_invariant (\forall int k; 0 <= k && k < i; a[k] == k);
         //@ decreases a.length -i;
         for (int i = 0; i < a.length; i++) {
             a[i] = i;
         }
     }
-*/
 
-/*
     //@ requires arr != null;
     //@ ensures \forall int k; 0 <= k && k < arr.length-1;arr[k] <= arr[k+1];
     public static void selectionSort(int[] arr) {
@@ -44,9 +42,17 @@ public class OpenJMLTest {
         }
     }
 
+    // @requires num != null;
+    // @requires pos >= 0 && pos < nums.length;
+    // @ensures \result >= pos && \result < nums.length;
+    // @ensures (\forall int k; k >= pos && k < nums.length; nums[\result] <= nums[k]);
     public static int minIndex(int[] nums, int pos) {
 
         int index = pos;
+
+        // @loop_invariant pos < i && i <= nums.length;
+        // @loop_invariant pos <= index && index <= nums.length;
+        // @loop_invariant (\forall int k; k >= pos && k < i; nums[index] <= nums[k]);
         for (int i = index+1; i < nums.length; i++) {
             if (nums[i] < nums[index]) {
                 index = i;
@@ -55,5 +61,4 @@ public class OpenJMLTest {
 
         return index;
     }
-*/
 }
--- src/finalMaterial/week15_2May_tues/Termination.java
+++ src/finalMaterial/week15_2May_tues/Termination.java
@@ -8,10 +8,26 @@ public class Termination {
         int sum = 0;
         int count = 0;
 
+        //measure of work: y-count iterations
+
         //@ loop_invariant sum == x*count;
+        //@ loop_invariant count <= y;
         while (count != y) {
+            int measure1 = y-count;
+
             sum = sum + x;
             count = count + 1;
+
+            int measure2 = y-count;
+
+            assert measure2 < measure1 : "less work to do with each iteration";
+
+            //when I have no work left, then my loop will be done
+            //!work -> done
+            //measure2 == 0 -> count == y
+
+            //work V done
+            assert measure2 != 0 || count == y : "I either have more work, or my loop will end";
         }
 
         return sum;
@@ -21,6 +37,9 @@ public class Termination {
     //@ensures \result == x*y;
     public static int multRec(int x, int y) {
         //what happens here if we don't enforce our precondition?
+        if (y < 0) {
+            throw new IllegalArgumentException("second parameter should be nonnegative");
+        }
 
         if (y == 0) {
             return 0;
@@ -31,12 +50,26 @@ public class Termination {
     }
 
     /*
-    How to show termination with recursive method?
+    How to show termination with recursive method? Use induction.
+    Base case: y = 0. Goes into if, returns and terminates.
+
+    Inductive step: assume that multRec(x, y) terminates for some
+    nonnegative integer y. We need to show that multRec(x, y+1)
+    also terminates. y+1 will be at least 1, so will go into the else.
+    It makes the recursive call multRec(x, y+1-1) which really
+    multRec(x, y). We assumed multRec(x,y) terminated, so
+    that means multRec(x, y+1) also terminates.
+
      */
 
     //@requires n > 0;
     //@ensures \result == 1;
     public static int collatz(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("parameter should be positive");
+        }
+
+        //cur = 52 -> 26 -> 13 -> 40 -> 20 -> 10 -> 5 -> 16 -> 8 -> 4 ->2 -> 1
 
         int cur = n;
         while (cur > 1) {
